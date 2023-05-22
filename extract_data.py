27b60d1c6cdc20df60e26e64181f99394010686b
Gabriel44200 <64473570+Gabriel44200@users.noreply.github.com>
2023-05-23 00:44:26 +0200

    Update extract_data.py

--- extract_data.py
+++ extract_data.py
@@ -10,17 +10,21 @@ station_status_response = requests.get(station_status_url)
 station_status_data = station_status_response.json()
 
 # Get the current date and time
-current_date = datetime.now().strftime("%Y-%m-%d")
+current_date = datetime.now().strftime("%d-%m-%Y")
 current_time = datetime.now().strftime("%H-%M-%S")
 
-# Create a folder with the current date if it doesn't exist
-folder_name = f"status_data_{current_date}"
-os.makedirs(folder_name, exist_ok=True)
+# Create the "capturas" folder if it doesn't exist
+capturas_folder = "capturas"
+os.makedirs(capturas_folder, exist_ok=True)
 
-# Save station status to a file with a timestamp
-file_name = f"station_status_{current_time}.json"
-file_path = os.path.join(folder_name, file_name)
+# Create a folder with the current date if it doesn't exist
+date_folder = os.path.join(capturas_folder, current_date)
+os.makedirs(date_folder, exist_ok=True)
+
+# Save station status to a file with the timestamp
+file_name = f"station_status_{current_date}_{current_time}.json"
+file_path = os.path.join(date_folder, file_name)
 with open(file_path, "w") as status_file:
     json.dump(station_status_data, status_file)
 
-print("Station status extraction completed. File saved in:", folder_name)
+print("Station status extraction completed. File saved in:", file_path)
